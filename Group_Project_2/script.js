39059bcb8c4fe929ce7cd3e2347453d5ddc8a62e
Akashgoyal55 <agoyal1@sfsu.edu>
2024-05-01 20:34:26 -0700

    adding files - changed everything

--- Group_Project_2/script.js
+++ Group_Project_2/script.js
@@ -1,8 +1,1 @@
-// Get the logo element
-const logo = document.getElementById('logo');
 
-// Add click event listener to the logo
-logo.addEventListener('click', function() {
-    // Toggle the border class
-    this.classList.toggle('border');
-});
